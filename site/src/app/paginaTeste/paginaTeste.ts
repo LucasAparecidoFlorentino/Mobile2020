eca94ccd924536b5306e876f6a0b40f639c5291e
Aluno <lucasflorentino_2000@hotmail.com>
2020-02-13 22:34:46 -0300

    Aula Angular

---
+++ site/src/app/paginaTeste/paginaTeste.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'paginaTeste',
+  templateUrl: "./paginaTeste.html",
+  styleUrls : ["./paginaTeste.css"]
+})
+export class paginaTesteComponent {
+  title = 'Pagina Teste';
+}
